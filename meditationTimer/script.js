a6f7ff2ef868b922960e49f4618ad9a8369620d3
TimOelrichs <toelris>
2020-12-21 16:21:29 +0100

    reset button funktioniert

--- meditationTimer/script.js
+++ meditationTimer/script.js
@@ -1,24 +1,46 @@
 window.onload = init;
 
 const startBtn = document.getElementById('startBtn');
-var minuten = 10;
+const resetBtn = document.getElementById('resetBtn');
+const timeBox = document.getElementById('timeBox');
+
+var minuten = 1;
+let endTime;
+var resume = false; 
+var timerID = 0;
+var percent = 0;
+
 
 function init() {
     startBtn.addEventListener('click', clickHandlerStart)
+    resetBtn.addEventListener('click', clickHandlerReset);
 }
 
 function clickHandlerStart() {
-    let endTime = Date.now() + minuten * 60000;
-    let timer = setInterval(() => { console.log(endTime - Date.now()) }, 500)
+    if(!resume) endTime = Date.now() + minuten * 60000;
+    timerID = setInterval(() => {
+        let seconds = endTime - Date.now();
+        timeBox.textContent = new Date(seconds).toUTCString().split("1970")[1].split("GMT")[0];
+        percent = endTime * seconds / 100_000_000 ;
+        console.log(percent)
+    }, 300)
     startBtn.textContent = "Stop"
     startBtn.removeEventListener('click', clickHandlerStart )
-    startBtn.addEventListener('click', () => clickHandlerStop(timer))
+    startBtn.addEventListener('click', clickHandlerStop)
 }
 
-function clickHandlerStop(timer) {
-    clearInterval(timer);
+function clickHandlerStop() {
+    if(timerID) clearInterval(timerID);
+    resume = true;
+    timerID = 0;
     startBtn.textContent = "Start"
-    startBtn.removeEventListener('click', () => clickHandlerStop(timer))
-    startBtn.addEventListener('click', clickHandlerStart)
+    startBtn.removeEventListener('click', clickHandlerStop);
+    startBtn.addEventListener('click', clickHandlerStart);
+}
+
+function clickHandlerReset() {
+    if (timerID) clickHandlerStop();
+    resume = false; 
+    timeBox.textContent = "00:01:00";
 }
 
