a7409bbb2fa973d95639bbc6ba4debeb44dfbe2b
TimOelrichs <toelris>
2020-12-21 14:43:06 +0100

    fertig gestellt

--- flexboxPlayground/script.js
+++ flexboxPlayground/script.js
@@ -2,12 +2,18 @@ let propertiesObj = {
     "flex-direction": ["row", "row-reverse", "column", "column-reverse"],
     "flex-wrap": ["nowrap", "wrap", "wrap-reverse"],
     "justify-content": ["flex-start", "flex-end", "center", "space-between", "space-around"],
-    "align-content": ["strech", "flex-start", "flex-end", "center", "space-between", "space-around"]
+    "align-content": ["stretch", "flex-start", "flex-end", "center", "space-between", "space-around"]
 }
+
+let colors = ["#49A69C", "#F2F2F2", "#F2911B", "#D9B5A0", "#A66A5D"];
 
 window.onload = init;
 const formcontainer = document.getElementById("formcontainer");
 const resultBox = document.getElementById('resultBox');
+const widthrange = document.getElementById('widthRange');
+const addBtn = document.getElementById('addBtn') 
+const rangeLabel = document.getElementById('rangeLabel');
+var sampleCounter = 0;
 
 function init() {
     
@@ -16,6 +22,13 @@ function init() {
         createRadioGroup(prop)
     }
 
+    widthrange.addEventListener('change', (event) => {
+        document.documentElement.style.setProperty('--card-width', `${event.target.value}%`);
+        rangeLabel.textContent = event.target.value + "%";
+    })
+
+    addBtn.addEventListener('click', () => { addSamples(2) })
+    addSamples(3)
 }
 
 function createRadioGroup(prop) {
@@ -46,3 +59,23 @@ function createRadioGroup(prop) {
     formcontainer.appendChild(div);
 }
 
+function addSamples(number) {
+    for (let index = 0; index < number; index++) {
+        let div = document.createElement('div');
+        div.innerHTML = `
+        <button id="close${sampleCounter}">X</button>
+        <p class="number">${sampleCounter}</p>
+        <h1 contenteditable="true">Edit me1</h1>
+        <p contenteditable="true">Edit me!</p>
+        </div>`
+       
+        div.id = "sample" + sampleCounter;
+        div.classList.add("sampleBox");
+        div.style.backgroundColor = colors[sampleCounter % colors.length]
+        
+        resultBox.appendChild(div);
+        document.getElementById(`close${sampleCounter++}`).addEventListener('click', (event) => {
+            resultBox.removeChild(event.target.parentNode)
+        })
+    }
+}
