e7b44548de2e74267abbcef45c1bb2ad0eb83ef4
TimOelrichs <toelris>
2021-01-29 09:30:42 +0100

    added Vue MainPage

--- speakerTimer/script.js
+++ speakerTimer/script.js
@@ -1,6 +1,7 @@
 
 var redner = {currentSpeaking: null, counter: 1};
 var timerId;
+let colors = ["#49A69C", "#F2F2F2", "#F2911B", "#D9B5A0", "#A66A5D"];
 
 window.onload = () => {
     document.getElementById("add").addEventListener('click', addRedner);
@@ -19,11 +20,38 @@ function addRedner(){
         txt.value = "";
         redner[id] = {name, elapsedTime: 0};
         
-        card.innerHTML = `${name}\t<span id="t${id}">0</span> Sek.\t<button id="b${id}"">Start</button>`
+        card.innerHTML = `<i class="material-icons">face</i><h1>${name}</h1><h2 id="t${id}">00:00:00</h2><button id="b${id}"">Start</button>`
         document.getElementById("liste").appendChild(card);
         let btn = document.getElementById(`b${id}`);
         btn.addEventListener('click', () => startHandler(id))
+        addBarToChart(id);
+    
 }
+
+function addBarToChart(id) {
+    let xmlns = "http://www.w3.org/2000/svg";
+
+    let chart = document.getElementById("chart");
+    //let g = document.createElement('g');
+    let g = document.createElementNS(xmlns, "g");
+    g.id = `bar${id}`;
+    //let rect = document.createElement('rect');
+    //g.appendChild(rect);
+    g.innerHTML = `<rect id='rect${id}' x="${(id*10) +50}" y="50" width="10" height="1"
+     fill='${colors[id % colors.length]}'/>`
+     
+    //rect.setAttribute('width', 40);
+    //rect.setAttribute('heigt', 19);
+    chart.appendChild(g);
+}
+
+function updateBarHeight(id) {
+    let xmlns = "http://www.w3.org/2000/svg";
+    let bar = document.getElementById(`rect${id}`);
+    bar.setAttributeNS(xmlns, "height", redner[id].current / 1000)
+}
+
+
 
 function startHandler(id){
     
@@ -56,14 +84,11 @@ function timer(id){
     let r = redner[id];
     let d = Date.now();
     if(r.startTime){
-        //r.current = ((d - r.startTime)) / 1000 + r.elapsedTime;
         r.current = ((d - r.startTime)) + r.elapsedTime;
     }else{
         r.startTime = d;
         r.current = r.elapsedTime;
     }
-    //document.getElementById(`t${id}`).innerText = r.current;
-
     document.getElementById(`t${id}`).innerText = new Date(r.current).toUTCString().split("1970")[1].split("GMT")[0];r.current;
-    
+    updateBarHeight(id);
 }
