26890eb91e693bac03ab9ca8b89a608a458d85cd
TimOelrichs <toelris>
2021-01-31 01:04:08 +0100

    improved editor

--- backend/php/editor.php
+++ backend/php/editor.php
@@ -16,49 +16,99 @@ if (!isset($_SESSION["login"]) || $_SESSION["login"] != 1) {
     }
 
     input {
+        height: 20px;
         margin: 1rem;
     }
+
+    select {
+        height: 20px;
+        width: 100px;
+    }
+
+    label {
+        padding: 5px;
+        width: 150px;
+    }
 </style>
-<label for="newheader">Neue Rubrik</label>
-<input type="text" name="newheader" id="newHeader"><button onclick="addHeader()">+</button>
-<label for="newsubheader">Neue Unterkategorie</label>
-<input type="text" name="newsubheader" id="newSubHeader"><button onclick="addSubHeader()">+</button>
-<form method="post">
-    <fieldset>
-        <legend>Select content area and add a new text:</legend>
 
-        <select name="top_header">
-            <option value="html">HTML</option>
-            <option value="css">CSS</option>
-            <option value="javascript">JavaScript</option>
-        </select>
-        <select name="sub_header">
-        </select>
+<div id="form">
+    <legend>Select content area and add a new text:</legend>
+    <select name="top_header">
+    </select>
+    <label for="newheader">Neue Kategorie:</label>
+    <input type="text" name="newheader" id="newHeader"><button onclick="addHeader()">+</button>
+    <br>
+    <select name="sub_header">
+    </select>
+    <label for="newsubheader">Neue Unterkategorie:</label>
+    <input type="text" name="newsubheader" id="newSubHeader">
+    <button onclick="addSubHeader()">+</button>
+    <textarea name="content"></textarea>
+    <textarea name="references"></textarea>
+    <input type="submit" value="Submit" id="submit">
+</div>
 
-        <textarea name="content"></textarea>
-        <textarea name="references"></textarea>
-        <input type="submit" value="Submit">
-    </fieldset>
-</form>
 <?PHP
 $file = '../data/data.json';
 $contents = file_get_contents($file);
 $json = json_decode($contents, true);
-
 ?>
+
 <script>
     let json = <?PHP echo json_encode($json) ?>;
     const top_header = document.querySelector('select[name="top_header"]');
     const sub_header = document.querySelector('select[name="sub_header"]');
-    top_header.addEventListener('change', e => {
+    const txt_NewHeader = document.getElementById("newHeader");
+    const txt_NewSubHeader = document.getElementById("newSubHeader");
+    const submit = document.getElementById("submit");
+
+    init();
+
+    function init() {
+        Object.keys(json).forEach(key => {
+            const option = document.createElement('option');
+            option.value = key;
+            option.innerText = key;
+            top_header.append(option);
+        })
+
+        updateSubheaders(top_header.value);
+
+        top_header.addEventListener('change', e => {
+            updateSubheaders(e.target.value)
+        });
+
+        submit.addEventListener('click', (event) => {
+            event.preventDefault();
+            fetch(new Request("./server.php"), {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-store',
+                body: JSON.stringify(Array.from(document.querySelectorAll('.form'))
+                    .reduce((json, input_field) => {
+                        json[input_field.id] = input_field.value;
+                        return json
+                    }, {})),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        }); <
+        /
+    })
+    }
+
+
+    function updateSubheaders(header) {
+        let category = header || top_header.value;
         sub_header.innerHTML = "";
-        Object.keys(json[e.target.value]).forEach(key => {
+        Object.keys(json[header]).forEach(key => {
             const option = document.createElement('option');
             option.value = key;
             option.innerText = key;
             sub_header.append(option);
         });
-    });
+    }
 
     function addHeader() {
         let input = document.getElementById("newHeader");
@@ -74,8 +124,13 @@ $json = json_decode($contents, true);
     }
 
     function addSubHeader() {
-        let input = document.getElementById("newSubHeader");
-        json[input.value] = {};
+        const option = document.createElement('option');
+        option.value = txt_NewSubHeader.value;
+        option.innerText = txt_NewSubHeader.value;
+        sub_header.append(option);
+        json[top_header.value][txt_NewSubHeader.value] = {};
+        updateSubheaders(top_header.value);
+        txt_NewSubHeader.value = "";
     }
 </script>
 <?PHP
