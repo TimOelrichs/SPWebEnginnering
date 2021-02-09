49932608fc329e35e738e080969c04324505440a
TimOelrichs <toelris>
2021-02-09 12:30:27 +0100

    init 9.1 and fixed bug in 8.2

--- solutions/8.2/8.2.js
+++ solutions/8.2/8.2.js
@@ -4,9 +4,9 @@ let div = document.getElementById('container')
 async function getAndConcatText() {
     let inpA = document.getElementById('inpA').value;
     let inpB = document.getElementById('inpB').value;
-    console.log(inpA)
 
-    let a, b = await Promise.all([
+    let a, b
+    [a,b]  = await Promise.all([
         (async () => await fetch(inpA).then(x => x.text()))(),
         (async () => await fetch(inpB).then(x => x.text()))(),
     ])
@@ -14,7 +14,7 @@ async function getAndConcatText() {
     document.getElementById('txta').innerText = a;
     document.getElementById('txtb').innerText = b;
 
-    a, b = a.split('\n'), b.split('\n');
+    [a, b] = [a.split('\n'), b.split('\n')];
     a =  a.length >= b.length ? a.map((e, i) => e + (b[i] || "")) : b.map((e, i) => (a[i] || "") + e)
     a = a.join("<br>")
     div.innerHTML = a;
